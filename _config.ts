be1ef1924c2ac1a55700d95fb9ebffb238ba0ab1
graef <simonjn45@gmail.com>
2024-06-20 18:13:48 +0200

    [fix]cssbug

--- _config.ts
+++ _config.ts
@@ -1,15 +1,13 @@
 import lume from "lume/mod.ts";
 import date from "lume/plugins/date.ts";
-import lightningCss from "lume/plugins/lightningcss.ts";
-import minifyHTML from "lume/plugins/minify_html.ts";
 import readingInfo from "lume/plugins/reading_info.ts";
 import codeHighlight from "lume/plugins/code_highlight.ts";
+import lightningCss from "lume/plugins/lightningcss.ts";
+import minifyHTML from "lume/plugins/minify_html.ts";
 
 const site = lume({ src: "src/" });
 site
   .use(date())
-  .use(lightningCss())
-  .use(minifyHTML())
   .use(readingInfo())
   .use(
     codeHighlight({
@@ -18,6 +16,8 @@ site
         path: "/_includes/style/ct.css",
       },
     })
-  );
+  )
+  .use(lightningCss())
+  .use(minifyHTML());
 
 export default site;
