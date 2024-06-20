921b520253de679d2f6382be199acf7296f74fec
graef <simonjn45@gmail.com>
2024-06-20 17:58:02 +0200

    [add]css[rm]scss[mod]vto

--- _config.ts
+++ _config.ts
@@ -1,10 +1,23 @@
 import lume from "lume/mod.ts";
 import date from "lume/plugins/date.ts";
-import sass from "lume/plugins/sass.ts";
+import lightningCss from "lume/plugins/lightningcss.ts";
 import minifyHTML from "lume/plugins/minify_html.ts";
 import readingInfo from "lume/plugins/reading_info.ts";
+import codeHighlight from "lume/plugins/code_highlight.ts";
 
 const site = lume({ src: "src/" });
-site.use(date()).use(sass()).use(minifyHTML()).use(readingInfo());
+site
+  .use(date())
+  .use(lightningCss())
+  .use(minifyHTML())
+  .use(readingInfo())
+  .use(
+    codeHighlight({
+      theme: {
+        name: "github-dark-dimmed",
+        path: "/_includes/style/ct.css",
+      },
+    })
+  );
 
 export default site;
