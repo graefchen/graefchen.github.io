df001256b54b1b641b00c9df5e284d5c3a415d6c
graef <simonjn45@gmail.com>
2024-10-20 00:15:42 +0200

    [fix]speedup

--- _config.ts
+++ _config.ts
@@ -1,7 +1,8 @@
 import lume from "lume/mod.ts";
 import date from "lume/plugins/date.ts";
+import inline from "lume/plugins/inline.ts";
 
 const site = lume({ src: "src/" });
-site.copy("u").copy("s.css").use(date());
+site.copy("u").use(date()).use(inline());
 
 export default site;
