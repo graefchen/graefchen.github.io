9c9d62283779cd7d4c07e90d67801f0a14443c00
graef <simonjn45@gmail.com>
2024-12-09 10:22:53 +0100

    feat: added markdown-it

--- _config.ts
+++ _config.ts
@@ -1,8 +1,10 @@
 import lume from "lume/mod.ts";
 import date from "lume/plugins/date.ts";
 import inline from "lume/plugins/inline.ts";
+import footnote from "npm:markdown-it-footnote";
 
-const site = lume({ src: "src/" });
+const markdown = { plugins:[footnote] };
+const site = lume({ src: "src/" },{ markdown });
 site.copy("u").use(date()).use(inline());
 
 export default site;
